452ebf32d742ee8316a81caad44fc6dc211c2faa
Barend <11941174+bvanrijn@users.noreply.github.com>
2019-12-05 17:43:37 +0100

    Use 'find ... -delete' instead of 'rm'

--- build/build.sh
+++ build/build.sh
@@ -5,4 +5,4 @@ mkdir -p out
 javac src/me/barend/generateavatar/*.java
 cd src
 jar cmf ../build/GenerateAvatar.mf ../out/GenerateAvatar.jar me/barend/generateavatar/*.class
-rm me/barend/generateavatar/*.class
+find me/barend/generateavatar -name '*.class' -delete
